979ba0dadb1e911078d4d13eebaba0b84ca29191
dinho <dinho@LAPTOP-D7O6TG3O>
2024-01-26 11:05:49 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,117 @@
+﻿
+
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool runProgram = true;
+
+            while (runProgram)
+            {
+                Console.WriteLine("Welcome to the calculator");
+                bool parseSuccessful;
+                int firstNum;
+                int secondNum;
+
+                
+
+                do
+                {
+                    Console.WriteLine("Enter first number");
+                    string num1 = Console.ReadLine();
+
+                    parseSuccessful = int.TryParse(num1, out firstNum);
+
+                    if (!parseSuccessful)
+                    {
+                        Console.WriteLine("Invalid input");
+                    }
+                } while (!parseSuccessful);
+
+                string operator1;
+                do
+                {
+                    Console.WriteLine("Enter an operator (+, -, * or /)");
+                    operator1 = Console.ReadLine();
+                } while (operator1 != "+" && operator1 != "-" && operator1 != "*" && operator1 != "/");
+
+                
+                do
+                {
+
+                    Console.WriteLine("Enter second number");
+                    string num2 = Console.ReadLine();
+
+
+                    parseSuccessful = int.TryParse(num2, out secondNum);
+
+                    if (!parseSuccessful)
+                    {
+                        Console.WriteLine("Invalid input");
+                    }
+                } while (!parseSuccessful);
+
+                Calculator calculator = new Calculator();
+
+                switch (operator1)
+                {
+                    case "+":
+                        Console.WriteLine($"Result: {calculator.Add(firstNum, secondNum)}");
+                        break;
+                    case "-":
+                        Console.WriteLine($"Result: {calculator.Sub(firstNum, secondNum)}");
+                        break;
+                    case "*":
+                        Console.WriteLine($"Result: {calculator.Mul(firstNum, secondNum)}");
+                        break;
+                    case "/":
+                        Console.WriteLine($"Result: {calculator.Div(firstNum, secondNum)}");
+                        break;
+                }
+
+                Console.WriteLine("Do you want to calculate again? Y/N");
+                string runAgainInput = Console.ReadLine();
+                if(runAgainInput.ToLower() == "y")
+                {
+                    runProgram = true;
+                }
+                else if (runAgainInput.ToLower() == "n")
+                {
+                    runProgram = false;
+                }
+            }
+        }
+    }
+
+    public class Calculator
+    {
+        public int Add(int firstNum, int secondNum)
+        {
+            return firstNum + secondNum;
+        }
+        public int Sub(int firstNum, int secondNum)
+        {
+            return firstNum - secondNum;
+        }
+        public int Mul(int firstNum, int secondNum)
+        {
+            return firstNum * secondNum;
+        }
+        public int Div(int firstNum, int secondNum)
+        {
+            if (secondNum != 0)
+            {
+                return firstNum / secondNum;
+            }
+            else
+            {
+                Console.WriteLine("Error: Division by zero.");
+                return 0;
+            }
+        }
+    }
+}
